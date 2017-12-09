2b19dfb952b9b3e4040e5523705c9c03106297fb
unknown <dws@yoyoma.com>
2017-12-09 15:38:18 -0500

    Removed test to cause coverage to not be 100%

--- tests/Multiply_Test.cpp
+++ tests/Multiply_Test.cpp
@@ -9,10 +9,7 @@
 #include "../src/Multiply.hpp"
 
 TEST(MultiplyTest, twoValues){
-	const int x = 4;
-	const int y = 5;
 	Multiply multiply;
-	EXPECT_EQ(20, multiply.twoValues(x,y));
 	EXPECT_EQ(6, multiply.twoValues(2, 3));
 }
 
